c0d4ed3b40d1e80b3c85d981650c9256befe77ac
LiNk-NY <marcel.ramosperez@roswellpark.org>
2023-03-09 16:13:01 -0500

    add include_recent argument for removing latest month of data

--- R/downloadTrend.R
+++ R/downloadTrend.R
@@ -1,5 +1,9 @@
 #' Plot the download activity for a package
 #'
+#' @param include_recent `logical(1)` Whether to include the latest month of
+#'   download data. Usually, the data for the most recent month has incomplete
+#'   numbers and is excluded (default is `FALSE`).
+#'
 #' @inheritParams generateReport
 #'
 #' @examples
@@ -8,21 +12,31 @@
 #' downloadTrend("cBioPortalData", "2017-05-01")
 #' downloadTrend("RaggedExperiment", "2017-05-01")
 #'
+#'
 #' downloadTrend("SingleCellMultiModal", "2017-05-01")
+#' downloadTrend("curatedTCGAData", "2017-05-01")
+#'
+#' # low downloads
+#' downloadTrend("TENxIO", "2017-05-01")
+#' downloadTrend("terraTCGAdata", "2017-05-01")
 #'
 #' @export
-downloadTrend <- function(package, since_date) {
+downloadTrend <- function(package, since_date, include_recent = FALSE) {
     if (requireNamespace("lubridate", quietly = TRUE))
         syear <- lubridate::year(lubridate::as_date(since_date))
     else
-        syear <- format(as.Date.character("2021-01-02"), "%Y")
-    now <- format(Sys.time(), "%Y")
+        syear <- format(as.Date.character(since_date), "%Y")
+    now <- Sys.time()
+    this_year <- format(now, "%Y")
+    this_month <- format(now, "%b")
     ## Average Downloads
     type <- .get_pkg_type(package)
     pkgType <- typeTranslate(type)
     dls <- BiocPkgTools::pkgDownloadStats(
         package, pkgType = pkgType, years = syear:now
     )
+    if (!include_recent)
+        dls <- dls[!(dls$Year == this_year & dls$Month == this_month), ]
     x <- seq(nrow(dls))
     y <- dls[["Nb_of_distinct_IPs"]]
     plot(
@@ -30,6 +44,7 @@ downloadTrend <- function(package, since_date) {
         xlab = "", ylab = "No. downloads (distinct IP)",
         main = paste0(package, ": Download Activity")
     )
+    ## basic selector of months
     vals <- x[c(TRUE, FALSE)]
     tickmo <- dls[["Month"]][c(TRUE, FALSE)]
     mtext(tickmo, side = 1, line = 1, at = vals, las = 2, cex = 0.8)
